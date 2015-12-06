f4b66dd1b43a0bd3f2c262e4d846d113b97527d8
rja0315 <rja0315@ubuntu>
2015-12-06 11:04:37 -0800

    fixed lazy implement. added desc by publishededdate, and size of list

--- src/main/java/com/rja0315/repository/ItemRepository.java
+++ src/main/java/com/rja0315/repository/ItemRepository.java
@@ -1,9 +1,14 @@
 package com.rja0315.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.rja0315.entity.Item;
+import com.rja0315.entity.Record;
 
 public interface ItemRepository extends JpaRepository<Item, Integer>{
 
+	List<Item> findByRecord(Record record, Pageable pageable);
 }
--- src/main/java/com/rja0315/repository/RecordRepository.java
+++ src/main/java/com/rja0315/repository/RecordRepository.java
@@ -1,9 +1,13 @@
 package com.rja0315.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.rja0315.entity.Record;
+import com.rja0315.entity.User;
 
 public interface RecordRepository extends JpaRepository<Record, Integer> {
 
+List<Record> findByUser(User user); 
 }
